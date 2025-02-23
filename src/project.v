5c8857e34bc86a892efed35b74e68f0ddf46dcb6
daobaanh <daobaanhvn1991@gmail.com>
2025-02-23 22:56:08 +0700

    adding trng source code, replace primitives with sky_cells

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_daobaanh_rng (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,22 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out[7:1]  = 7'b0;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{rst_n, 1'b0};
+
+  // ring_osc part
+  top_TRNG TRNG_inst(
+    .clk_sys(clk),
+    .i_en(ena),
+    .RX_Serial(ui_in[0]),
+    .TX_Serial(uo_out[0]),
+    .o_RO(uo_out[1]),
+    .o_RG(uo_out[2]),
+    .led(uo_out[3]));
+
 
 endmodule
